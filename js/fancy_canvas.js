0423a8b15874117a17570bc88c905c2fd2b56028
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-04-03 20:58:47 +0200

    Remove ttest

--- js/fancy_canvas.js
+++ js/fancy_canvas.js
@@ -20,8 +20,8 @@ function updateCanvas() {
 	const millisecondsPassed = date.getHours() * 60 * 60 * 1000 + date.getMinutes() * 60 * 1000 + date.getSeconds() * 1000 + date.getMilliseconds();
 	const t = millisecondsPassed / millisecondsInADay;
 	c.clearRect(0, 0, w, h);
-	const tTest = (date.getSeconds() * 1000 + date.getMilliseconds()) / (1000 * 60);
-	draw(c, w, h, tTest);
+	// const tTest = (date.getSeconds() * 1000 + date.getMilliseconds()) / (1000 * 60);
+	draw(c, w, h, t);
 }
 
 if (typeof draw === "undefined") {
